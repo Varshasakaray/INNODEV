8a36dbf5f2dc672435e702e8d008112dd04ee31e
Rakshith <rakshithkothuru@gmail.com>
2024-03-15 01:31:39 +0530

    update

--- main.php
+++ main.php
@@ -26,15 +26,15 @@
         <h5 class="card-header">Typing Tools</h5>
         <div class="card-body">
           
-          <a href="main.php?lang=0" class="btn btn-primary col-3 m-2">English Typing</a>
-          <a href="main.php?lang=1" class="btn btn-primary col-3 m-2">Telugu Typing</a>
-          <a href="main.php?lang=2" class="btn btn-primary col-3 m-2">Hindi Typing</a>
-          <a href="main.php?lang=3" class="btn btn-primary col-3 m-2">Punjabi Typing</a>
-          <a href="main.php?lang=4" class="btn btn-primary col-3 m-2">Malyalam Typing</a>
-          <a href="main.php?lang=5" class="btn btn-primary col-3 m-2">Kannada Typing</a>
-          <a href="main.php?lang=6" class="btn btn-primary col-3 m-2">Marathi Typing</a>
-          <a href="main.php?lang=7" class="btn btn-primary col-3 m-2">Tamil Typing</a>
-          <a href="main.php?lang=8" class="btn btn-primary col-3 m-2">Urdu Typing</a>
+          <a href="typingtool.php?lang=0" class="btn btn-primary col-3 m-2">English Typing</a>
+          <a href="typingtool.php?lang=1" class="btn btn-primary col-3 m-2">Telugu Typing</a>
+          <a href="typingtool.php?lang=2" class="btn btn-primary col-3 m-2">Hindi Typing</a>
+          <a href="typingtool.php?lang=3" class="btn btn-primary col-3 m-2">Punjabi Typing</a>
+          <a href="typingtool.php?lang=4" class="btn btn-primary col-3 m-2">Malyalam Typing</a>
+          <a href="typingtool.php?lang=5" class="btn btn-primary col-3 m-2">Kannada Typing</a>
+          <a href="typingtool.php?lang=6" class="btn btn-primary col-3 m-2">Marathi Typing</a>
+          <a href="typingtool.php?lang=7" class="btn btn-primary col-3 m-2">Tamil Typing</a>
+          <a href="typingtool.php?lang=8" class="btn btn-primary col-3 m-2">Urdu Typing</a>
           
 
         </div>
