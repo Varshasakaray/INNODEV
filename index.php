e89ae362309454748ee7f2b9922a14ee02137755
Rakshith <rakshithkothuru@gmail.com>
2024-03-17 02:32:09 +0530

    update

--- index.php
+++ index.php
@@ -1,58 +1,75 @@
 <?php
-     include('php/db1.php');
+   session_start();
+   include("php/db1.php");
+
+   if($_SERVER['REQUEST_METHOD']=="POST")
+   {
+     $username=$_POST['username'];
+     $password=$_POST['pass'];
+ 
+     if(!empty($username) && !empty($password) && !is_numeric($username))
+     {
+       
+      $query="select * from register where username='$username' limit 1";
+      $result= mysqli_query($con,$query);
+
+      if($result)
+      {
+        if($result && mysqli_num_rows($result)> 0)
+        {
+          $user_data=mysqli_fetch_assoc($result);
+            
+          if($user_data['password']== $password)
+          {
+            header("location: mainpage.php");
+            die;
+          }
 
 
-     ?>
 
+        }
+      }
+      echo"<script type='text/javascript'> alert('wrong username or password')</script>";
 
-
-
+     }
+     else{
+      echo"<script type='text/javascript'> alert('wrong username or password')</script>";
+     }
+   }
+?>
 
 <!DOCTYPE html>
-<html>
-  
+<html lang="en">
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>speedyfingers-learn and improve </title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h1></h1>
-    <nav class="navbar navbar-dark bg-dark">
-        <div class="container">
-          <a class="navbar-brand" href="#">
-            <img src="image/logo.webp" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-            Test your speed
-          </a>
-        </div>
-      </nav>
-      <div class="box">
-        <p>innodev project </p>
-      </div>
+    <div class="box">
+      <form method="POST" action="">
+          <h2>Login</h2>
+          <div class="InputBox">
+            <input type="text" name="username" class="nonrectangle" required>
+            <span>username</span>
+            <i></i> 
 
-      <div class="container">
-      <div class="card mt-4">
-        <h5 class="card-header">Typing Tools</h5>
-        <div class="card-body">
-          
-          <a href="main.php?lang=0" class="btn btn-primary col-3 m-2">English Typing</a>
-          <a href="main.php?lang=1" class="btn btn-primary col-3 m-2">Telugu Typing</a>
-          <a href="main.php?lang=2" class="btn btn-primary col-3 m-2">Hindi Typing</a>
-          <a href="main.php?lang=3" class="btn btn-primary col-3 m-2">Punjabi Typing</a>
-          <a href="main.php?lang=4" class="btn btn-primary col-3 m-2">Malyalam Typing</a>
-          <a href="main.php?lang=5" class="btn btn-primary col-3 m-2">Kannada Typing</a>
-          <a href="main.php?lang=6" class="btn btn-primary col-3 m-2">Marathi Typing</a>
-          <a href="main.php?lang=7" class="btn btn-primary col-3 m-2">Tamil Typing</a>
-          <a href="main.php?lang=8" class="btn btn-primary col-3 m-2">Urdu Typing</a>
-          
+          </div>
+          <div class="InputBox">
+            <input type="password" name="pass" class="nonrectangle" required>
+            <span>Enter Password</span>
+            <i></i> 
 
-        </div>
-      </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+          </div>
+          <input type="submit" value="Login">
+          <div class="links">
+            <a href="#">Forgot Password?</a>
+            <a href="signup.php">SignUp</a>
+            <div>
+
+        </form>
+    <div>    
 </body>
 </html>
